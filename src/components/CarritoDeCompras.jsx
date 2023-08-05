0c9c4d7de8c8b71b00a543c0f8e5b557485a9c8a
Luis Miguel Rodriguez <rodriguezmujica883@gmail.com>
2023-08-05 16:07:14 -0400

    1.0

---
+++ src/components/CarritoDeCompras.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Carrito = () => 
+<> 
+<span><img className='CarritoC' src={require('../utilities/carrito-de-compras.png')} alt="carrito" />
+0</span>
+</>
+
+export default Carrito;
---
+++ src/components/logo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Logo() {
+  return (
+    <img className='logos' src={require("../utilities/Logos.jpg")} alt="logo" />
+  );
+}
